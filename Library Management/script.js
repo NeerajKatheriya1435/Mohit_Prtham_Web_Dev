2651ec0c5ee4893077a6fd5a415a81c61f3ad10e
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-07-21 12:28:34 +0530

    project add delete update query

---
+++ Library Management/script.js
@@ -0,0 +1,77 @@
+let mybooks=JSON.parse(localStorage.getItem("books")) || []
+let editIndex = -1;
+
+function saveNote(){
+    let bookName=document.getElementById("bookName").value.trim()
+    let bookAuthor=document.getElementById("bookAuthor").value.trim()
+    
+    if(bookName==="" || bookAuthor===""){
+        alert("Please fill the entry")
+        return;
+    }
+
+    if(editIndex===-1){
+        mybooks.push({
+            book:bookName,
+            author:bookAuthor
+        })
+    }
+    else{
+        mybooks[editIndex].book=bookName
+        mybooks[editIndex].author=bookAuthor
+        editIndex=-1
+        document.getElementById("saveBtn").innerHTML="Add Book"
+    }
+
+    localStorage.setItem("books", JSON.stringify(mybooks));
+    clearForm()
+    displayBooks()
+}
+
+function displayBooks(){
+    let table=""
+    mybooks.forEach((item,index)=>{
+        table+=`
+        <tr>
+            <td>${index + 1}</td>
+            <td>${item.book}</td>
+            <td>${item.author}</td>
+
+            <td>
+
+                <button class="edit"
+                onclick="editBook(${index})">
+                Edit
+                </button>
+
+                <button class="delete"
+                onclick="deleteBook(${index})">
+                Delete
+                </button>
+            </td>
+        </tr>
+        `
+    });
+    document.getElementById("bookManager").innerHTML = table;
+}
+
+// UPDATE
+function editBook(index) {
+
+    document.getElementById("bookName").value = mybooks[index].book;
+
+    document.getElementById("bookAuthor").value = mybooks[index].author;
+
+    editIndex = index;
+
+    document.getElementById("saveBtn").innerText = "Update Book";
+
+}
+// Clear Form
+function clearForm() {
+
+    document.getElementById("bookName").value = "";
+    document.getElementById("bookAuthor").value = "";
+
+}
+
